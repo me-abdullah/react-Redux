2d67664e4c6b61d786a092ba9ca6640fc2775a16
me-abdullah <enzoemrick@gmail.com>
2024-11-10 23:19:05 +0500

    Practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions/index';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container mt-5'>
+        <div className='text-center'>
+          <h2 className='mb-4'>Increment/Decrement Counter</h2>
+          <h3 className='mb-4'>React Redux</h3>
+
+          <div className='d-flex justify-content-center align-items-center'>
+            <button 
+              type='button' 
+              className='btn btn-danger mx-2' 
+              title='Decrement' 
+              onClick={() => dispatch(decNumber())}
+            >
+              -
+            </button>
+            <input 
+              name='quantity' 
+              type='text' 
+              className='form-control text-center' 
+              value={counter} 
+              readOnly 
+              style={{ width: '60px' }}
+            />
+            <button 
+              type='button' 
+              className='btn btn-success mx-2' 
+              title='Increment' 
+              onClick={() => dispatch(incNumber())}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
